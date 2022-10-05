67c30f9f999c4221e61121378b6cd373d2af8deb
김지훈 <pawn@192.168.nate.com>
2022-10-05 09:12:11 +0900

    init: project init

---
+++ src/test/java/com/pawn/autodistributedlock/AutoDistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawn.autodistributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDistributedLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
