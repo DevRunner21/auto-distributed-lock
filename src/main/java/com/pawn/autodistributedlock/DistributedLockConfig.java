960c05f8495bbed9cbab68cf815b2c30a4d977f9
김지훈 <pawn@Pawn-MacBookPro.local>
2022-10-09 17:48:34 +0900

    chore: spring 버전 Down -> 2.3.9

--- src/main/java/com/pawn/autodistributedlock/DistributedLockConfig.java
+++ src/main/java/com/pawn/autodistributedlock/DistributedLockConfig.java
@@ -1,10 +1,14 @@
 package com.pawn.autodistributedlock;
 
 import org.redisson.api.RedissonClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableCaching
+@EnableAutoConfiguration
 public class DistributedLockConfig {
 
     @Bean
